e251d1d83c0225f31787ed3f116efa4603fc2869
1sisodiyaji <637golusingh@gmail.com>
2024-03-02 09:49:45 +0530

    white board rtrial

--- src/pages/Whiteboard.jsx
+++ src/pages/Whiteboard.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import io from "socket.io-client";
+import ClientRoom from "./whiteboard/ClientRoom";
+import JoinCreateRoom from "./whiteboard/JoinCreateRoom";
+import Room from "./whiteboard/Room";
+import Sidebar from "./whiteboard/sidebar";
+
+
+const server = "https://server-zepw.onrender.com/";
+const connectionOptions = {
+  "force new connection": true,
+  reconnectionAttempts: "Infinity",
+  timeout: 10000,
+  transports: ["websocket"],
+};
+
+const socket = io(server, connectionOptions);
 
 const Whiteboard = () => {
-  return (
-    <div>Whiteboard</div>
-  )
-}
+  const [userNo, setUserNo] = useState(0);
+  const [roomJoined, setRoomJoined] = useState(false);
+  const [user, setUser] = useState({});
+  const [users, setUsers] = useState([]);
 
-export default Whiteboard
+  const uuid = () => {
+    const S4 = () =>
+      (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+
+    return `${S4()}${S4()}-${S4()}-${S4()}-${S4()}-${S4()}${S4()}${S4()}`;
+  };
+
+  useEffect(() => {
+    if (roomJoined) {
+      socket.emit("user-joined", user);
+    }
+  }, [roomJoined, user]);
+
+  return (
+    <div className="home">
+      <ToastContainer />
+      {roomJoined ? (
+        <>
+          <Sidebar users={users} user={user} socket={socket} />
+          {user.presenter ? (
+            <Room
+              userNo={userNo}
+              user={user}
+              socket={socket}
+              setUsers={setUsers}
+              setUserNo={setUserNo}
+            />
+          ) : (
+            <ClientRoom
+              userNo={userNo}
+              user={user}
+              socket={socket}
+              setUsers={setUsers}
+              setUserNo={setUserNo}
+            />
+          )}
+        </>
+      ) : (
+        <JoinCreateRoom
+          uuid={uuid}
+          setRoomJoined={setRoomJoined}
+          setUser={setUser}
+        />
+      )}
+    </div>
+  );
+};
+
+export default Whiteboard;
