63ed6ab58b063ed95e5bd0306afa42e6b7350f18
1sisodiyaji <golu@wooble.org>
2024-03-03 20:16:51 +0530

    commit

--- src/pages/Whiteboard.jsx
+++ src/pages/Whiteboard.jsx
@@ -1,74 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { toast, ToastContainer } from "react-toastify";
-import io from "socket.io-client";
-import ClientRoom from "./whiteboard/ClientRoom";
-import JoinCreateRoom from "./whiteboard/JoinCreateRoom";
-import Room from "./whiteboard/Room";
-import Sidebar from "./whiteboard/sidebar";
-
-
-const server = "https://server-zepw.onrender.com/";
-const connectionOptions = {
-  "force new connection": true,
-  reconnectionAttempts: "Infinity",
-  timeout: 10000,
-  transports: ["websocket"],
-};
-
-const socket = io(server, connectionOptions);
+import React from 'react'
 
 const Whiteboard = () => {
-  const [userNo, setUserNo] = useState(0);
-  const [roomJoined, setRoomJoined] = useState(false);
-  const [user, setUser] = useState({});
-  const [users, setUsers] = useState([]);
-
-  const uuid = () => {
-    const S4 = () =>
-      (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-
-    return `${S4()}${S4()}-${S4()}-${S4()}-${S4()}-${S4()}${S4()}${S4()}`;
-  };
-
-  useEffect(() => {
-    if (roomJoined) {
-      socket.emit("user-joined", user);
-    }
-  }, [roomJoined, user]);
-
   return (
-    <div className="home " style={{minHeight:'100vh'}}>
-      <ToastContainer />
-      {roomJoined ? (
-        <>
-          
-          {user.presenter ? (
-            <Room
-              userNo={userNo}
-              user={user}
-              socket={socket}
-              setUsers={setUsers}
-              setUserNo={setUserNo}
-            />
-          ) : (
-            <ClientRoom
-              userNo={userNo}
-              user={user}
-              socket={socket}
-              setUsers={setUsers}
-              setUserNo={setUserNo}
-            />
-          )}
-        </>
-      ) : (
-        <JoinCreateRoom
-          uuid={uuid}
-          setRoomJoined={setRoomJoined}
-          setUser={setUser}
-        />
-      )}
-    </div>
-  );
-};
+    <div>Whiteboard</div>
+  )
+}
 
-export default Whiteboard;
+export default Whiteboard
