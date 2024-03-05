1d79f8e9ca8d0556cce873a7336097fec824120e
1sisodiyaji <golu@wooble.org>
2024-03-05 23:35:57 +0530

    c

--- src/index.js
+++ src/index.js
@@ -1,25 +1,32 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
-import  {hydrate , render} from "react-dom"
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <GoogleOAuthProvider clientId= "1095610353417-7m3ijrgcftibi74skie2i7pecugk47jd.apps.googleusercontent.com">
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </GoogleOAuthProvider>
-);
-
+import App from "./App";
 
 const rootElement = document.getElementById('root');
-if(rootElement.hasChildNodes()){
-  hydrate(App,rootElement);
-}else{
-  render(App,rootElement);
+
+if (rootElement.hasChildNodes()) {
+  ReactDOM.hydrate(
+    <React.StrictMode>
+      <GoogleOAuthProvider clientId="1095610353417-7m3ijrgcftibi74skie2i7pecugk47jd.apps.googleusercontent.com">
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </GoogleOAuthProvider>
+    </React.StrictMode>,
+    rootElement
+  );
+} else {
+  ReactDOM.render(
+    <React.StrictMode>
+      <GoogleOAuthProvider clientId="1095610353417-7m3ijrgcftibi74skie2i7pecugk47jd.apps.googleusercontent.com">
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </GoogleOAuthProvider>
+    </React.StrictMode>,
+    rootElement
+  );
 }
