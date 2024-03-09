548745f0b9ae63fbfce1d21e96c9f75a93164400
1sisodiyaji <93850014+1sisodiyaji@users.noreply.github.com>
2024-03-10 01:45:54 +0530

    commit

--- src/component/NewsApi.js
+++ src/component/NewsApi.js
@@ -21,10 +21,10 @@ const NewsApi = () => {
   return (
     <>
     <h5 className='text-success'>Trends On Today World</h5>
-    <div style={{ display: 'flex', flexWrap: 'wrap', overflowY:'scroll' ,height:'100vh', borderBottom:'1px solid #FFE164'}}>
-      {data.map(item => (
-        <div key={item.id} className='card p-2 my-1' style={{backgroundColor:'#031A33' , border:'1px solid #79b4e2'}}>
-          <h6 style={{color:'#FFE164'}}>{item.title}</h6>
+    <div style={{ display: 'flex', flexWrap: 'wrap', overflowY: 'scroll', height: '100vh', borderBottom: '1px solid #FFE164' }}>
+      {data && data.map(item => (
+        <div key={item.id} className='card p-2 my-1' style={{ backgroundColor: '#031A33', border: '1px solid #79b4e2' }}>
+          <h6 style={{ color: '#FFE164' }}>{item.title}</h6>
           <small className='text-light'>{item.description}</small>
           <div className="row">
             <div className="col-6"><Link to={item.url} className="btn btn-sm ms-2 text-secondary border border-warning" target='blank'>Read it</Link></div>
@@ -33,7 +33,7 @@ const NewsApi = () => {
         </div>
       ))}
     </div>
-    </>
+  </>
   );
 };
 
