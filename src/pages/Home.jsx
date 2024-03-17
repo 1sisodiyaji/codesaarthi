9d256bccefe5d7e4f92a66ec3a4151e4fc8c38bd
1sisodiyaji <93850014+1sisodiyaji@users.noreply.github.com>
2024-03-17 17:37:08 +0530

    trial

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -50,123 +50,10 @@ const Home = () => {
 
               <div className="row g-6 p-2">
 
-                <div className="col-lg-4 col-12 my-2 ">
-                  <div class=" card  tilt-effect2 " style={{ backgroundColor: '#6aa1b4' }}>
-                    <div class="card-body">
-                      <h5 class="card-title">Solutions Of Top 50 Questions </h5>
-                      <p class="card-text">
-                        DSA on Array and Strings ...
-                      </p>
-
-                      <button type="button" class="btn btn-warning">Start Solving</button>
-                    </div>
-                  </div>
-                </div>
-
-
-
-                <div className="col-lg-4 col-12 my-2">
-                  <div class=" card  tilt-effect2 " style={{ backgroundColor: '#6aa1b4' }}>
-                    <div class="card-body">
-                      <h5 class="card-title">Top Notch Notes </h5>
-                      <p class="card-text">
-                        Easiest Way to understand Dsa Topics
-                      </p>
-                      <button type="button" class="btn btn-primary">View</button>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="col-lg-4 col-12 my-2">
-                  <div class="card  tilt-effect2" style={{ backgroundColor: '#6aa1b4' }}>
-                    <div class="card-body">
-                      <h5 class="card-title">DBMS Notes</h5>
-                      <p class="card-text">
-                        Learn DBMS Concepts in simple Words..
-                      </p>
-
-                      <button type="button" class="btn btn-success">Get It</button>
-                    </div>
-                  </div>
-                </div>
-
-              </div>
-
-              <h3 className='text-capitalize text-dark'>Study plans </h3>
-              <div className="row g-6 p-2">
-
-                <div className="col-lg-4 col-12 my-2">
-                  <div class=" card  tilt-effect2 " style={{ backgroundColor: '#6aa1b4' }}>
-                    <div class="card-body">
-                      <div className="row">
-                        <div className="col-4">
-                          <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-                            <img src="../../img/logo.png" style={{ height: '70px' }} class="img-fluid" />
-                            <a href="#!">
-                              <div class="mask" style={{ backgroundColor: 'rgba(251, 251, 251, 0.15' }}></div>
-                            </a>
-                          </div>
-                        </div>
-                        <div className="col-8 text-center align-self-center">
-                          <h6 class="card-title">W3 SChools Solutions</h6>
-                          <div className="btn bg-warning text-dark">Get It</div>
-                        </div>
-
-                      </div>
-
-                    </div>
-                  </div>
-                </div>
-
-                <div className="col-lg-4 col-12 my-2 ">
-                  <div class="card  tilt-effect2" style={{ backgroundColor: '#6aa1b4' }}>
-                    <div class="card-body">
-                      <div className="row">
-                        <div className="col-4">
-                          <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-                            <img src="../../img/logo.png" style={{ height: '70px' }} class="img-fluid" />
-                            <a href="#!">
-                              <div class="mask" style={{ backgroundColor: 'rgba(251, 251, 251, 0.15' }}></div>
-                            </a>
-                          </div>
-                        </div>
-                        <div className="col-8 text-center align-self-center">
-                          <h6 class="card-title">Bit Manipulations Targetted Question</h6>
-                          <div className="btn bg-warning text-dark">Get It</div>
-                        </div>
-
-                      </div>
-
-                    </div>
-                  </div>
-                </div>
-
-                <div className="col-lg-4 col-12 my-2">
-                  <div class=" card  tilt-effect2 " style={{ backgroundColor: '#6aa1b4' }}>
-                    <div class="card-body">
-                      <div className="row">
-                        <div className="col-4">
-                          <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-                            <img src="../../img/logo.png" style={{ height: '70px' }} class="img-fluid" />
-                            <a href="#!">
-                              <div class="mask" style={{ backgroundColor: 'rgba(251, 251, 251, 0.15' }}></div>
-                            </a>
-                          </div>
-                        </div>
-                        <div className="col-8 text-center align-self-center">
-                          <h6 class="card-title"> 10 Pattern Questions</h6>
-                          <div className="btn bg-warning text-dark">Get It</div>
-                        </div>
-
-                      </div>
-
-                    </div>
-                  </div>
-                </div>
+               <Problems/>
 
               </div>
             </div>
-
           </div>
         </div>
         <div className="col-lg-3 d-lg-block d-none d-flex justify-content-center align-items-center">
@@ -218,14 +105,6 @@ const Home = () => {
           <img src="img/mision1.png" className="img-fluid" alt="" />
         </div>
       </div>
-
-
-
-
-
-
-
-      <AboutUs />
 
     </>
   );
