527aba215c276969f951952d07110648c5bcc561
1sisodiyaji <93850014+1sisodiyaji@users.noreply.github.com>
2024-03-17 11:47:41 +0530

    check

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React from 'react';
 
 const Profile = () => {
+    const pic = localStorage.getItem("user_ProfilePic");
     return (
         <>
             <div className="container d-flex justify-content-center align-items-center g-0" style={{ minHeight: '100vh' }}>
                 <div className="card p-3 g-0" style={{ height: '500px', width: '420px' , backgroundColor:'#3393df' , boxShadow:'5px 5px 10px #000435' }}>
                     <div className='d-flex justify-content-center align-items-center' >
                         <div style={{borderRadius:'50%' , border:'5px solid green'}} className='p-1'>
-                        <img src="img/logo.png" className="img-fluid" style={{ height: '25vh' }} />
+                        <img src= {pic ? (
+                        <>
+                          <img
+                            src={pic}
+                            className="rounded-circle"
+                            height="25"
+                            alt="Black and White Portrait of a Man"
+                            loading="lazy"
+                          />
+                        </>
+                      ) : (
+                        <>
+                        <div className="border border-success"> 
+                          <i className="fi fi-ss-user"></i>
+                          </div>
+                        </>
+                      )} className="img-fluid" style={{ height: '25vh' }} />
                         </div>
                     </div>
 
---
+++ src/pages/Theory/Dbms/basic_dbms.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const basic_dbms = () => {
+    
+  return (
+    <>
+    
+    
+    </>
+  )
+}
+
+export default basic_dbms
