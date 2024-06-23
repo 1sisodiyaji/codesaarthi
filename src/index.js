1c27bbb61f75f7fc0cf6c7aec69ae21eeb52bf04
1sisodiyaji <637golusingh@gmail.com>
2024-06-23 15:33:47 +0530

    vercel speed

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <GoogleOAuthProvider clientId= "1095610353417-7m3ijrgcftibi74skie2i7pecugk47jd.apps.googleusercontent.com">
       <BrowserRouter>
         <App />
+        <SpeedInsights />
       </BrowserRouter>
     </GoogleOAuthProvider>
 );
