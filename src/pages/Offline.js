eb16e80476c7d8160ef1341ca05f0c1925041706
1sisodiyaji <637golusingh@gmail.com>
2024-06-23 15:30:20 +0530

    update

--- src/pages/Offline.js
+++ src/pages/Offline.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Offline = () => {
   return (
-    <div>Offline</div>
+    <>
+    <div className="container-fluid vh-100 bg-light d-flex justify-content-center align-items-center">
+        <h6 className='text-danger'> Please turn on your Internet for further surfing ...</h6>
+        <img src="img/offline.png" alt="offline page images" loading='lazy' className='img-fluid' title='offline bro please turn on your internet' />
+    </div>
+    
+    </>
   )
 }
 
