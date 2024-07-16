aff934ec689c442fd43a1e60e8b6f8b4292a4cde
Golu_singh <637golusingh@gmail.com>
2024-07-16 12:41:34 +0530

    .

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client"; 
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import { GoogleOAuthProvider } from '@react-oauth/google'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
