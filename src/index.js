937626e39638f458e6c3730b84cb8d8b09e59560
Golu_singh <637golusingh@gmail.com>
2024-08-03 16:41:10 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import { Provider, useDispatch } from 'react-redux';
 import store from './store/store';
 import getIdFromToken from "./config/getIdfromToken";
 import { loginSuccess } from './store/actions/userAction'; // Correct import
-
+import { SpeedInsights } from '@vercel/speed-insights/react';
+ 
 const Root = () => {
   const dispatch = useDispatch();
 
@@ -31,6 +32,7 @@ root.render(
     <GoogleOAuthProvider clientId="1095610353417-7m3ijrgcftibi74skie2i7pecugk47jd.apps.googleusercontent.com">
       <BrowserRouter>
         <Root />
+        <SpeedInsights />
       </BrowserRouter>
     </GoogleOAuthProvider>
   </Provider>
