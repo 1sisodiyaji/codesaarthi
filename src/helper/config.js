4ad7383b54aca7ff4727db29146355fc9bea8686
Golu_singh <637golusingh@gmail.com>
2024-12-23 15:52:47 +0530

    commit

--- src/helper/config.js
+++ src/helper/config.js
@@ -1,9 +1,9 @@
 const config = { 
 
-    BASE_URL: 'http://localhost:8081'
+    // BASE_URL: 'http://localhost:8081'
     
    
-    // BASE_URL: "https://server-beta-lovat-71.vercel.app"
+    BASE_URL: "https://server-beta-lovat-71.vercel.app"
 
     
     // BASE_URL: ' https://server-8dy1.onrender.com'
